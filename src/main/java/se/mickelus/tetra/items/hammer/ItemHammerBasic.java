a1aa11441bd1ff73bdacd87472b6b70fb3c5a088
Mikael E. Wikner <mikael.wikner@codemill.se>
2017-12-08 01:06:34 +0100

    added capabilities to basic hammer

--- src/main/java/se/mickelus/tetra/items/hammer/ItemHammerBasic.java
+++ src/main/java/se/mickelus/tetra/items/hammer/ItemHammerBasic.java
@@ -1,9 +1,16 @@
 package se.mickelus.tetra.items.hammer;
 
+import net.minecraft.item.ItemStack;
+import se.mickelus.tetra.capabilities.Capability;
+import se.mickelus.tetra.capabilities.ICapabilityProvider;
 import se.mickelus.tetra.items.TetraCreativeTabs;
 import se.mickelus.tetra.items.TetraItem;
 
-public class ItemHammerBasic extends TetraItem {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ItemHammerBasic extends TetraItem implements ICapabilityProvider {
     private static final String unlocalizedName = "hammer_basic";
 
     public ItemHammerBasic() {
@@ -13,5 +20,19 @@ public class ItemHammerBasic extends TetraItem {
         setMaxDamage(200);
         setMaxStackSize(1);
         setCreativeTab(TetraCreativeTabs.getInstance());
+    }
+
+    @Override
+    public int getCapabilityLevel(ItemStack itemStack, Capability capability) {
+        if (capability == Capability.hammer) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public Collection<Capability> getCapabilities(ItemStack itemStack) {
+        return Collections.singletonList(Capability.hammer);
     }
 }
