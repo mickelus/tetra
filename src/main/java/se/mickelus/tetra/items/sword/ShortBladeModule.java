4a178ae8fd6767a53596bf030b65433994eafdb5
Mikael E. Wikner <mikael.wikner@codemill.se>
2018-03-22 13:40:38 +0100

    added textures for shortblade improvements

--- src/main/java/se/mickelus/tetra/items/sword/ShortBladeModule.java
+++ src/main/java/se/mickelus/tetra/items/sword/ShortBladeModule.java
@@ -9,27 +9,34 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.CombatRules;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import org.apache.commons.lang3.ArrayUtils;
 import se.mickelus.tetra.DataHandler;
 import se.mickelus.tetra.PotionBleeding;
+import se.mickelus.tetra.TetraMod;
 import se.mickelus.tetra.items.ItemModularHandheld;
 import se.mickelus.tetra.module.ItemModuleMajor;
 import se.mickelus.tetra.module.ItemUpgradeRegistry;
 import se.mickelus.tetra.module.HandheldModuleData;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class ShortBladeModule extends ItemModuleMajor<HandheldModuleData> {
 
     public static final String key = "sword/short_blade";
 
-    public static final String hookedImprovement = key + ".hooked";
-    public static final String temperedImprovement = key + ".tempered";
-    public static final String serratedImprovement = key + ".serrated";
+    public static final String hookedImprovement = "short_blade/hooked";
+    public static final String temperedImprovement = "short_blade/tempered";
+    public static final String serratedImprovement = "short_blade/serrated";
 
     public static ShortBladeModule instance;
 
     public ShortBladeModule(String slotKey) {
         super(slotKey, key);
 
-        data = DataHandler.instance.getModuleData(key, WeaponModuleData[].class);
+        data = DataHandler.instance.getModuleData(key, HandheldModuleData[].class);
 
         improvements = new String[] {
             hookedImprovement,
@@ -72,19 +79,24 @@ public class ShortBladeModule extends ItemModuleMajor<HandheldModuleData> {
 
     private void backstabEntity(ItemStack itemStack, EntityLivingBase target, EntityLivingBase attacker) {
         if (180 - Math.abs(Math.abs(attacker.rotationYawHead - target.rotationYawHead) - 180) < 60) {
+            // + 1 as it is the default bonus damage dealt by players
+            // todo: use damage attribute from attacker
+            float damage = (float) (ItemModularHandheld.getDamageModifierStatic(itemStack) + 1) * 2;
             attacker.world.playSound(null, attacker.posX, attacker.posY, attacker.posZ,
                 SoundEvents.ENTITY_PLAYER_ATTACK_CRIT, attacker.getSoundCategory(), 1.0F, 1.0F);
             if (attacker instanceof EntityPlayer) {
                 ((EntityPlayer) attacker).onCriticalHit(target);
-                target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), 1);
+                target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), damage);
             } else {
-                target.attackEntityFrom(DamageSource.causeMobDamage(attacker), 1);
+                target.attackEntityFrom(DamageSource.causeMobDamage(attacker), damage);
             }
         }
     }
 
     private void penetrateEntityArmor(ItemStack itemStack, EntityLivingBase target, EntityLivingBase attacker) {
         if (target.getLastDamageSource() != null && attacker.equals(target.getLastDamageSource().getTrueSource())) {
+            // + 1 as it is the default bonus damage dealt by players
+            // todo: use damage attribute from attacker
             float damage = (float) (ItemModularHandheld.getDamageModifierStatic(itemStack) + 1);
             float reducedDamage = CombatRules.getDamageAfterAbsorb(damage, (float)target.getTotalArmorValue(), (float)target.getEntityAttribute(SharedMonsterAttributes.ARMOR_TOUGHNESS).getAttributeValue());
             if (reducedDamage < damage / 2) {
@@ -97,6 +109,25 @@ public class ShortBladeModule extends ItemModuleMajor<HandheldModuleData> {
                 }
             }
         }
+    }
 
+    @Override
+    public ResourceLocation[] getAllTextures() {
+        return Stream.concat(
+                Arrays.stream(super.getAllTextures()),
+                Arrays.stream(improvements)
+                        .map(improvement -> "items/" + improvement)
+                        .map(resourceString -> new ResourceLocation(TetraMod.MOD_ID, resourceString)))
+                .toArray(ResourceLocation[]::new);
+    }
+
+    @Override
+    public ResourceLocation[] getTextures(ItemStack itemStack) {
+        return Stream.concat(
+                Arrays.stream(super.getTextures(itemStack)),
+                Arrays.stream(getImprovements(itemStack))
+                        .map(improvement -> "items/" + improvement)
+                        .map(resourceString -> new ResourceLocation(TetraMod.MOD_ID, resourceString)))
+                .toArray(ResourceLocation[]::new);
     }
 }
