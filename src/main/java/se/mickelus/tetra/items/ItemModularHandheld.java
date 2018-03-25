75692597428724b04390cbc9ae1cd45b27067c18
Mikael E. Wikner <mikael.wikner@codemill.se>
2018-03-26 01:31:09 +0200

    possibly fixed incorrect/slow harvest speed for some blocks

--- src/main/java/se/mickelus/tetra/items/ItemModularHandheld.java
+++ src/main/java/se/mickelus/tetra/items/ItemModularHandheld.java
@@ -1,14 +1,20 @@
 package se.mickelus.tetra.items;
 
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import se.mickelus.tetra.NBTHelper;
 
 import javax.annotation.Nullable;
@@ -18,6 +24,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ItemModularHandheld extends ItemModular {
+
+    private static final Set<Block> axeBlocks = Sets.newHashSet(Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
+    private static final Set<Material> axeMaterials = Sets.newHashSet(Material.WOOD, Material.PLANTS, Material.VINE);
+
+    private static final Set<Block> pickaxeBlocks = Sets.newHashSet(Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB, Blocks.STONE_BUTTON, Blocks.STONE_PRESSURE_PLATE);
+    private static final Set<Material> pickaxeMaterials = Sets.newHashSet(Material.IRON, Material.ANVIL, Material.ROCK);
+
 
     @Override
     public boolean onBlockDestroyed(ItemStack itemStack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
@@ -90,9 +103,9 @@ public class ItemModularHandheld extends ItemModular {
     }
 
     public double getSpeedModifier(ItemStack itemStack) {
-        if (isBroken(itemStack)) {
-            return 2;
-        }
+//        if (isBroken(itemStack)) {
+//            return 2;
+//        }
 
         double speedModifier = getAllModules(itemStack).stream()
             .map(itemModule -> itemModule.getSpeedModifier(itemStack))
@@ -144,8 +157,21 @@ public class ItemModularHandheld extends ItemModular {
     @Override
     public float getDestroySpeed(ItemStack itemStack, IBlockState blockState) {
         if (!isBroken(itemStack)) {
-            float speed = (float) (4 + getSpeedModifier(itemStack)) *
-                    getCapabilityEfficiency(itemStack, blockState.getBlock().getHarvestTool(blockState));
+            String tool = blockState.getBlock().getHarvestTool(blockState);
+
+            if (tool == null) {
+                if (axeMaterials.contains(blockState.getMaterial())) {
+                    tool = "axe";
+                } else if (pickaxeMaterials.contains(blockState.getMaterial())) {
+                    tool = "pickaxe";
+                } else if (axeBlocks.contains(blockState.getBlock())) {
+                    tool = "axe";
+                } else if (pickaxeBlocks.contains(blockState.getBlock())) {
+                    tool = "pickaxe";
+                }
+            }
+
+            float speed = (float) (4 + getSpeedModifier(itemStack)) * getCapabilityEfficiency(itemStack, tool);
 
             if (speed < 1) {
                 return 1;
