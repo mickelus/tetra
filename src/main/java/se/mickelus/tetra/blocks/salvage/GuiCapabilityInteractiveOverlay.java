46a5bcd414292c2a0c4ca9bbd1cb3208442251f1
Mikael E. Wikner <wikner.mikael@gmail.com>
2018-11-28 18:10:57 +0100

    added out transitions to block interaction outlines

--- src/main/java/se/mickelus/tetra/blocks/salvage/GuiCapabilityInteractiveOverlay.java
+++ src/main/java/se/mickelus/tetra/blocks/salvage/GuiCapabilityInteractiveOverlay.java
@@ -4,7 +4,15 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import se.mickelus.tetra.capabilities.CapabilityHelper;
+import se.mickelus.tetra.gui.GuiElement;
+import se.mickelus.tetra.gui.animation.Applier;
+import se.mickelus.tetra.gui.animation.KeyframeAnimation;
 import se.mickelus.tetra.gui.hud.GuiRootHud;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class GuiCapabilityInteractiveOverlay extends GuiRootHud {
 
@@ -13,16 +21,41 @@ public class GuiCapabilityInteractiveOverlay extends GuiRootHud {
 
     }
 
-    public void update(IBlockState blockState, EnumFacing face, EntityPlayer player) {
-        clearChildren();
+    public void update(IBlockState blockState, EnumFacing face, EntityPlayer player, boolean transition) {
+
+
         if (blockState.getBlock() instanceof IBlockCapabilityInteractive) {
             IBlockCapabilityInteractive block = (IBlockCapabilityInteractive) blockState.getBlock();
 
             BlockInteraction[] interactions = block.getPotentialInteractions(blockState, face, CapabilityHelper.getPlayerCapabilities(player));
 
-            for (BlockInteraction interaction : interactions) {
-                GuiInteractiveOutline outline = new GuiInteractiveOutline(interaction);
-                addChild(outline);
+            if (transition) {
+                Collection<GuiInteractiveOutline> previousOutlines = elements.stream()
+                        .filter(element -> element instanceof GuiInteractiveOutline)
+                        .map(element -> (GuiInteractiveOutline) element)
+                        .collect(Collectors.toCollection(LinkedList::new));
+
+                // animate out outlines for all interactions which are no longer possible
+                previousOutlines.stream()
+                        .filter(outline -> Arrays.stream(interactions)
+                                .noneMatch(interaction -> interaction.equals(outline.getBlockInteraction())))
+                        // todo: add methods for removing and listing children?
+                        .forEach(outline -> outline.transitionOut(outline::remove));
+
+                // add and animate outlines for all new interactions
+                Arrays.stream(interactions)
+                        .filter(interaction -> previousOutlines.stream()
+                                .map(GuiInteractiveOutline::getBlockInteraction)
+                                .noneMatch(interaction::equals))
+                        .map(GuiInteractiveOutline::new)
+                        .forEach(this::addChild);
+            } else {
+                clearChildren();
+
+                Arrays.stream(interactions)
+                        .map(GuiInteractiveOutline::new)
+                        .forEach(this::addChild);
+
             }
         }
 
--- src/main/java/se/mickelus/tetra/blocks/salvage/GuiInteractiveOutline.java
+++ src/main/java/se/mickelus/tetra/blocks/salvage/GuiInteractiveOutline.java
@@ -6,16 +6,25 @@ import se.mickelus.tetra.gui.animation.KeyframeAnimation;
 
 public class GuiInteractiveOutline extends GuiElement {
 
-    private static final String texture = "textures/gui/workbench.png";
+    private static final String texture = "textures/gui/block_interaction.png";
+
+    private BlockInteraction blockInteraction;
+
+    private GuiTexture topLeft;
+    private GuiTexture topRight;
+    private GuiTexture bottomLeft;
+    private GuiTexture bottomRight;
 
     public GuiInteractiveOutline(BlockInteraction blockInteraction) {
         super((int) blockInteraction.minX, (int) blockInteraction.minY,
                 (int) (blockInteraction.maxX - blockInteraction.minX),
                 (int) (blockInteraction.maxY - blockInteraction.minY));
 
+        this.blockInteraction = blockInteraction;
+
         opacity = 0.3f;
 
-        GuiTexture topLeft = new GuiTexture(-2, -2, 4, 4, 68, 23, texture);
+        topLeft = new GuiTexture(-2, -2, 4, 4, 0, 0, texture);
         addChild(topLeft);
         new KeyframeAnimation(100, topLeft)
                 .applyTo(new Applier.Opacity(0, 1),
@@ -24,7 +33,7 @@ public class GuiInteractiveOutline extends GuiElement {
                 .withDelay(500)
                 .start();
 
-        GuiTexture topRight = new GuiTexture(2, -2, 4, 4, 71, 23, texture);
+        topRight = new GuiTexture(2, -2, 4, 4, 3, 0, texture);
         topRight.setAttachment(GuiAttachment.topRight);
         addChild(topRight);
         new KeyframeAnimation(100, topRight)
@@ -34,7 +43,7 @@ public class GuiInteractiveOutline extends GuiElement {
                 .withDelay(650)
                 .start();
 
-        GuiTexture bottomLeft = new GuiTexture(-2, 2, 4, 4, 71, 23, texture);
+        bottomLeft = new GuiTexture(-2, 2, 4, 4, 3, 0, texture);
         bottomLeft.setAttachment(GuiAttachment.bottomLeft);
         addChild(bottomLeft);
         new KeyframeAnimation(100, bottomLeft)
@@ -44,7 +53,7 @@ public class GuiInteractiveOutline extends GuiElement {
                 .withDelay(500)
                 .start();
 
-        GuiTexture bottomRight = new GuiTexture(2, 2, 4, 4, 68, 23, texture);
+        bottomRight = new GuiTexture(2, 2, 4, 4, 0, 0, texture);
         bottomRight.setAttachment(GuiAttachment.bottomRight);
         addChild(bottomRight);
         new KeyframeAnimation(100, bottomRight)
@@ -52,6 +61,37 @@ public class GuiInteractiveOutline extends GuiElement {
                         new Applier.TranslateX(0, 2),
                         new Applier.TranslateY(0, 2))
                 .withDelay(650)
+                .start();
+    }
+
+    public BlockInteraction getBlockInteraction() {
+        return blockInteraction;
+    }
+
+    public void transitionOut(Runnable onStop) {
+        new KeyframeAnimation(200, topLeft)
+                .applyTo(new Applier.Opacity(1, 0),
+                        new Applier.TranslateX(-5),
+                        new Applier.TranslateY(-5))
+                .start();
+
+        new KeyframeAnimation(200, topRight)
+                .applyTo(new Applier.Opacity(1, 0),
+                        new Applier.TranslateX(5),
+                        new Applier.TranslateY(-5))
+                .start();
+
+        new KeyframeAnimation(200, bottomLeft)
+                .applyTo(new Applier.Opacity(1, 0),
+                        new Applier.TranslateX(-5),
+                        new Applier.TranslateY(5))
+                .start();
+
+        new KeyframeAnimation(200, bottomRight)
+                .applyTo(new Applier.Opacity(1, 0),
+                        new Applier.TranslateX(5),
+                        new Applier.TranslateY(5))
+                .onStop(finished -> onStop.run())
                 .start();
     }
 }
