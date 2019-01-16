a450d2e26888448b4f29dfb5d5fb6fe26c6cf273
Mikael E. Wikner <wikner.mikael@gmail.com>
2019-01-16 02:00:22 +0100

    added tooltips describing unmet capability requirements

--- src/main/java/se/mickelus/tetra/blocks/workbench/gui/GuiCapabilityRequirement.java
+++ src/main/java/se/mickelus/tetra/blocks/workbench/gui/GuiCapabilityRequirement.java
@@ -1,9 +1,16 @@
 package se.mickelus.tetra.blocks.workbench.gui;
 
+import net.minecraft.client.resources.I18n;
 import se.mickelus.tetra.capabilities.Capability;
 import se.mickelus.tetra.gui.*;
 
+import java.util.Collections;
+import java.util.List;
+
 public class GuiCapabilityRequirement extends GuiCapability {
+
+    int requiredLevel;
+    int availableLevel;
 
     public GuiCapabilityRequirement(int x, int y, Capability capability) {
         super(x, y, capability);
@@ -11,6 +18,8 @@ public class GuiCapabilityRequirement extends GuiCapability {
 
     public void updateRequirement(int requiredLevel, int availableLevel) {
         setVisible(requiredLevel != 0);
+        this.requiredLevel = requiredLevel;
+        this.availableLevel = availableLevel;
 
         if (isVisible()) {
             if (requiredLevel > availableLevel) {
@@ -19,5 +28,13 @@ public class GuiCapabilityRequirement extends GuiCapability {
                 update(requiredLevel, GuiColors.add);
             }
         }
+    }
+
+    @Override
+    public List<String> getTooltipLines() {
+        if (hasFocus() && requiredLevel > availableLevel) {
+            return Collections.singletonList(I18n.format(capability + ".unavailable", requiredLevel));
+        }
+        return super.getTooltipLines();
     }
 }
