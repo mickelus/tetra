362abe62e133d21de22df508d18519d2602ec9d5
Mikael E. Wikner <wikner.mikael@gmail.com>
2019-03-15 22:38:40 +0100

    reordered schema types

--- src/main/java/se/mickelus/tetra/module/schema/SchemaType.java
+++ src/main/java/se/mickelus/tetra/module/schema/SchemaType.java
@@ -5,6 +5,11 @@ package se.mickelus.tetra.module.schema;
  */
 public enum SchemaType {
     /**
+     * Renders in the same way as major, but with a + int he bottom left. Use for improvement schemas.
+     */
+    improvement,
+
+    /**
      * Renders the same outline as shown around minor modules, use for minor module schemas.
      */
     minor,
@@ -13,11 +18,6 @@ public enum SchemaType {
      * Renders a similar outline as major modules, but cut off at the top and the bottom. Use for major module schemas.
      */
     major,
-
-    /**
-     * Renders in the same way as major, but with a + int he bottom left. Use for improvement schemas.
-     */
-    improvement,
 
     /**
      * Renders no outline. Currently unused ¯\_(ツ)_/¯
