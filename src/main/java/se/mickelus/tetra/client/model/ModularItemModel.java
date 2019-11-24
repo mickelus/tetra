e88b911666d9378802fc155ff453a9e0a0faf80a
Mikael E. Wikner <mikael.wikner@codemill.se>
2019-11-24 01:06:12 +0100

    cleaned up modular model

--- src/main/java/se/mickelus/tetra/client/model/ModularItemModel.java
+++ src/main/java/se/mickelus/tetra/client/model/ModularItemModel.java
@@ -1,25 +1,20 @@
 package se.mickelus.tetra.client.model;
 
 import javax.annotation.Nullable;
-import javax.vecmath.Vector4f;
 
 import net.minecraft.client.renderer.model.*;
 import net.minecraft.client.renderer.texture.ISprite;
 import net.minecraftforge.client.model.BakedItemModel;
-import net.minecraftforge.client.model.ItemTextureQuadConverter;
 import net.minecraftforge.client.model.PerspectiveMapWrapper;
-import net.minecraftforge.versions.forge.ForgeVersion;
 
 import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
-import net.minecraft.resources.IResourceManager;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.pipeline.IVertexConsumer;
 import net.minecraftforge.client.model.pipeline.TRSRTransformer;
 import net.minecraftforge.client.model.pipeline.UnpackedBakedQuad;
-import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.common.model.TRSRTransformation;
 
 import java.util.*;
@@ -105,181 +100,43 @@ public final class ModularItemModel implements IUnbakedModel {
     public static List<BakedQuad> getQuadsForSprite(int tint, TextureAtlasSprite sprite, VertexFormat format, Optional<TRSRTransformation> transform) {
         ImmutableList.Builder<BakedQuad> builder = ImmutableList.builder();
 
-//        int uMax = sprite.getWidth();
-//        int vMax = sprite.getHeight();
-//
-//        FaceData faceData = new FaceData(uMax, vMax);
-//        boolean anyTransparent = false;
-//
-//        for(int f = 0; f < sprite.getFrameCount(); f++) {
-//            boolean transparentWest;
-//            boolean[] transparentUp = new boolean[uMax];
-//            Arrays.fill(transparentUp, true);
-//            for(int v = 0; v < vMax; v++) {
-//                transparentWest = true;
-//                for(int u = 0; u < uMax; u++) {
-//                    int alpha = sprite.getPixelRGBA(f, u, v) >> 24 & 0xFF;
-//                    boolean transparent = alpha / 255f <= 0.1f;
-//
-//                    if (!transparent && alpha < 255) {
-//                        anyTransparent = true;
-//                    }
-//
-//                    if(transparentWest && !transparent){ // left - transparent, right - opaque
-//                        faceData.set(Direction.WEST, u, v);
-//                    }
-////                    if(!transparentWest && transparent){ // left - opaque, right - transparent
-//                        faceData.set(Direction.EAST, u, v);
-////                    }
-////                    if(transparentUp[u] && !transparent){ // up - transparent, down - opaque
-//                        faceData.set(Direction.UP, u, v);
-////                    }
-////                    if(!transparentUp[u] && transparent){ // up - opaque, down - transparent
-//                        faceData.set(Direction.DOWN, u, v);
-////                    }
-//
-//                    transparentWest = transparent;
-//                    transparentUp[u] = transparent;
-//                }
-//
-//                if(!transparentWest){ // last - opaque
-//                    faceData.set(Direction.EAST, uMax-1, v);
-//                }
-//            }
-//            // last line
-//            for(int u = 0; u < uMax; u++){
-//                if(!transparentUp[u]){
-//                    faceData.set(Direction.DOWN, u, vMax-1);
-//                }
-//            }
-//        }
-//
-//        // horizontal quads
-//        for (Direction facing : HORIZONTALS) {
-//            for (int v = 0; v < vMax; v++) {
-//                int uStart = 0, uEnd = uMax;
-//                boolean building = false;
-//                for (int u = 0; u < uMax; u++) {
-//                    boolean face = faceData.get(facing, u, v);
-//                    if (!anyTransparent) {
-//                        if (face) {
-//                            if (!building) {
-//                                building = true;
-//                                uStart = u;
-//                            }
-//                            uEnd = u + 1;
-//                        }
-//                    } else {
-//                        if (building && !face) { // finish current quad
-//                            // make quad [uStart, u]
-//                            int off = facing == Direction.DOWN ? 1 : 0;
-//                            builder.add(buildSideQuad(format, transform, facing, tint, sprite, uStart, v + off, u - uStart));
-//                            building = false;
-//                        } else if (!building && face) { // start new quad
-//                            building = true;
-//                            uStart = u;
-//                        }
-//                    }
-//                }
-//                if (building) { // build remaining quad
-//                    // make quad [uStart, uEnd]
-//                    int off = facing == Direction.DOWN ? 1 : 0;
-//                    builder.add(buildSideQuad(format, transform, facing, tint, sprite, uStart, v+off, uEnd-uStart));
-//                }
-//            }
-//
-//        }
+        int uMax = sprite.getWidth();
+        int vMax = sprite.getHeight();
 
-        // vertical quads
-//        for (Direction facing : VERTICALS) {
-//            for (int u = 0; u < uMax; u++) {
-//                int vStart = 0, vEnd = vMax;
-//                boolean building = false;
-//                for (int v = 0; v < vMax; v++) {
-//                    boolean face = faceData.get(facing, u, v);
-//                    if (!anyTransparent) {
-//                        if (face) {
-//                            if (!building) {
-//                                building = true;
-//                                vStart = v;
-//                            }
-//                            vEnd = v + 1;
-//                        }
-//                    }
-//                    else {
-//                        if (building && !face) { // finish current quad
-//                            // make quad [vStart, v]
-//                            int off = facing == Direction.EAST ? 1 : 0;
-//                            builder.add(buildSideQuad(format, transform, facing, tint, sprite, u+off, vStart, v-vStart));
-//                            building = false;
-//                        }
-//                        else if (!building && face) { // start new quad
-//                            building = true;
-//                            vStart = v;
-//                        }
-//                    }
-//                }
-//                if (building) { // build remaining quad
-//                    // make quad [vStart, vEnd]
-//                    int off = facing == Direction.EAST ? 1 : 0;
-//                    builder.add(buildSideQuad(format, transform, facing, tint, sprite, u+off, vStart, vEnd-vStart));
-//                }
-//            }
-//        }
+        // todo 1.14: figure out why this doesn't work when considering fully transparent rows
+        for (int v = 0; v < vMax; v++) {
+            builder.add(buildSideQuad(format, transform, Direction.UP, tint, sprite, 0, v, uMax));
+            builder.add(buildSideQuad(format, transform, Direction.DOWN, tint, sprite, 0, v + 1, uMax));
+        }
+
+        for (int u = 0; u < uMax; u++) {
+            builder.add(buildSideQuad(format, transform, Direction.EAST, tint, sprite, u + 1, 0, vMax));
+            builder.add(buildSideQuad(format, transform, Direction.WEST, tint, sprite, u, 0, vMax));
+        }
 
         // front
-        builder.add(buildQuad(format, transform, Direction.NORTH, sprite, tint,
+        builder.add(buildQuad(format, transform, Direction.NORTH, sprite, tint, 0xffffffff,
                 0, 0, 7.5f / 16f, sprite.getMinU(), sprite.getMaxV(),
                 0, 1, 7.5f / 16f, sprite.getMinU(), sprite.getMinV(),
                 1, 1, 7.5f / 16f, sprite.getMaxU(), sprite.getMinV(),
                 1, 0, 7.5f / 16f, sprite.getMaxU(), sprite.getMaxV()
         ));
         // back
-        builder.add(buildQuad(format, transform, Direction.SOUTH, sprite, tint,
+        builder.add(buildQuad(format, transform, Direction.SOUTH, sprite, tint, 0xffffffff,
                 0, 0, 8.5f / 16f, sprite.getMinU(), sprite.getMaxV(),
                 1, 0, 8.5f / 16f, sprite.getMaxU(), sprite.getMaxV(),
                 1, 1, 8.5f / 16f, sprite.getMaxU(), sprite.getMinV(),
                 0, 1, 8.5f / 16f, sprite.getMinU(), sprite.getMinV()
         ));
 
-//        builder.addAll(ItemTextureQuadConverter.convertTexture(format, transform.orElse(TRSRTransformation.identity()),
-//                sprite, sprite, 0, Direction.EAST, 0xFFFFFFFF, tint));
-//
         return builder.build();
     }
 
-    private static class FaceData {
-        private final EnumMap<Direction, BitSet> data = new EnumMap<>(Direction.class);
 
-        private final int vMax;
+    private static BakedQuad buildSideQuad(VertexFormat format, Optional<TRSRTransformation> transform, Direction side, int tintIndex,
+            TextureAtlasSprite sprite, int u, int v, int size) {
+        int color = 0xffffffff;
 
-        FaceData(int uMax, int vMax)
-        {
-            this.vMax = vMax;
-
-            data.put(Direction.WEST, new BitSet(uMax * vMax));
-            data.put(Direction.EAST, new BitSet(uMax * vMax));
-            data.put(Direction.UP,   new BitSet(uMax * vMax));
-            data.put(Direction.DOWN, new BitSet(uMax * vMax));
-        }
-
-        public void set(Direction facing, int u, int v)
-        {
-            data.get(facing).set(getIndex(u, v));
-        }
-
-        public boolean get(Direction facing, int u, int v)
-        {
-            return data.get(facing).get(getIndex(u, v));
-        }
-
-        private int getIndex(int u, int v)
-        {
-            return v * vMax + u;
-        }
-    }
-
-    private static BakedQuad buildSideQuad(VertexFormat format, Optional<TRSRTransformation> transform, Direction side, int tint, TextureAtlasSprite sprite, int u, int v, int size) {
         final float eps = 1e-2f;
 
         int width = sprite.getWidth();
@@ -287,20 +144,29 @@ public final class ModularItemModel implements IUnbakedModel {
 
         float x0 = (float) u / width;
         float y0 = (float) v / height;
-        float x1 = x0, y1 = y0;
-        float z0 = 7.5f / 16f, z1 = 8.5f / 16f;
 
-        switch(side)
-        {
+        float x1 = x0;
+        float y1 = y0;
+
+        float z0 = 7.5f / 16f;
+        float z1 = 8.5f / 16f;
+
+        switch(side) {
             case WEST:
                 z0 = 8.5f / 16f;
                 z1 = 7.5f / 16f;
+
+                y1 = (float) (v + size) / height;
+                break;
             case EAST:
                 y1 = (float) (v + size) / height;
                 break;
             case DOWN:
                 z0 = 8.5f / 16f;
                 z1 = 7.5f / 16f;
+
+                x1 = (float) (u + size) / width;
+                break;
             case UP:
                 x1 = (float) (u + size) / width;
                 break;
@@ -317,7 +183,7 @@ public final class ModularItemModel implements IUnbakedModel {
         float v1 = 16f * (1f - y1 - dy);
 
         return buildQuad(
-                format, transform, remap(side), sprite, tint,
+                format, transform, remap(side), sprite, tintIndex, color,
                 x0, y0, z0, sprite.getInterpolatedU(u0), sprite.getInterpolatedV(v0),
                 x1, y1, z0, sprite.getInterpolatedU(u1), sprite.getInterpolatedV(v1),
                 x1, y1, z1, sprite.getInterpolatedU(u1), sprite.getInterpolatedV(v1),
@@ -331,24 +197,24 @@ public final class ModularItemModel implements IUnbakedModel {
     }
 
     private static BakedQuad buildQuad(VertexFormat format, Optional<TRSRTransformation> transform, Direction side,
-            TextureAtlasSprite sprite, int tint,
+            TextureAtlasSprite sprite, int tintIndex, int color,
             float x0, float y0, float z0, float u0, float v0,
             float x1, float y1, float z1, float u1, float v1,
             float x2, float y2, float z2, float u2, float v2,
             float x3, float y3, float z3, float u3, float v3) {
         UnpackedBakedQuad.Builder builder = new UnpackedBakedQuad.Builder(format);
 
-        builder.setQuadTint(tint);
+        builder.setQuadTint(tintIndex);
         builder.setQuadOrientation(side);
         builder.setTexture(sprite);
 
         boolean hasTransform = transform.isPresent() && !transform.get().isIdentity();
         IVertexConsumer consumer = hasTransform ? new TRSRTransformer(builder, transform.get()) : builder;
 
-        putVertex(consumer, format, side, x0, y0, z0, u0, v0, 0xFFFFFFFF);
-        putVertex(consumer, format, side, x1, y1, z1, u1, v1, 0xFFFFFFFF);
-        putVertex(consumer, format, side, x2, y2, z2, u2, v2, 0xFFFFFFFF);
-        putVertex(consumer, format, side, x3, y3, z3, u3, v3, 0xFFFFFFFF);
+        putVertex(consumer, format, side, x0, y0, z0, u0, v0, color);
+        putVertex(consumer, format, side, x1, y1, z1, u1, v1, color);
+        putVertex(consumer, format, side, x2, y2, z2, u2, v2, color);
+        putVertex(consumer, format, side, x3, y3, z3, u3, v3, color);
 
         return builder.build();
     }
