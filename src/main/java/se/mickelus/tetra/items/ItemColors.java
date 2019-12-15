695828898eba41f10acf7470ac1bae30d4d7e9c4
Mikael E. Wikner <wikner.mikael@gmail.com>
2019-12-15 03:34:41 +0100

    moved shortblades to use new model setup

--- src/main/java/se/mickelus/tetra/items/ItemColors.java
+++ src/main/java/se/mickelus/tetra/items/ItemColors.java
@@ -64,6 +64,8 @@ public class ItemColors {
     public static int fierymetal_glyph  = define(0xfa9f00, "fierymetal_glyph");
     public static int knightmetal       = define(0xb8cf9c, "knightmetal");
     public static int knightmetal_glyph = define(0xb8cf9c, "knightmetal_glyph");
+    public static int prismarine        = define(0x8cffdb, "prismarine");
+    public static int prismarine_glyph  = define(0x73b5aa, "prismarine_glyph");
 
     public static int inherit           = define(0x000000, "inherit");
 
