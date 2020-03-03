ce5b71f577d2726a301d811086769d592da48bb1
Mikael E. Wikner <wikner.mikael@gmail.com>
2020-03-03 01:10:44 +0100

    fixed bleeding effect not working

--- src/main/java/se/mickelus/tetra/effects/BleedingEffect.java
+++ src/main/java/se/mickelus/tetra/effects/BleedingEffect.java
@@ -1,8 +1,10 @@
 package se.mickelus.tetra.effects;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
+import net.minecraft.util.DamageSource;
 
 public class BleedingEffect extends Effect {
     public static BleedingEffect instance;
@@ -12,6 +14,16 @@ public class BleedingEffect extends Effect {
         setRegistryName("bleeding");
 
         instance = this;
+    }
+
+    @Override
+    public void performEffect(LivingEntity entity, int amplifier) {
+        entity.attackEntityFrom(DamageSource.GENERIC, amplifier);
+    }
+
+    @Override
+    public boolean isReady(int duration, int amplifier) {
+        return duration % 10 == 0;
     }
 
     @Override
