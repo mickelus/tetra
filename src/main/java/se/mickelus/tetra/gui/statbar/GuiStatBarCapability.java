cfa85b317de9fe275248eb7c1712c93235f4a471
Mikael E. Wikner <wikner.mikael@gmail.com>
2020-03-06 00:53:47 +0100

    added indicators for striking & sweeping harvest effects

--- src/main/java/se/mickelus/tetra/gui/statbar/GuiStatBarCapability.java
+++ src/main/java/se/mickelus/tetra/gui/statbar/GuiStatBarCapability.java
@@ -2,20 +2,30 @@ package se.mickelus.tetra.gui.statbar;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import se.mickelus.mgui.gui.GuiAlignment;
+import se.mickelus.mgui.gui.GuiTexture;
+import se.mickelus.tetra.TetraMod;
 import se.mickelus.tetra.blocks.workbench.gui.GuiCapability;
 import se.mickelus.tetra.capabilities.Capability;
 import se.mickelus.tetra.gui.GuiColors;
+import se.mickelus.tetra.gui.GuiTextures;
 import se.mickelus.tetra.gui.statbar.getter.*;
 import se.mickelus.tetra.items.modular.ItemModular;
+import se.mickelus.tetra.module.ItemEffect;
 import se.mickelus.tetra.util.CastOptional;
 
 public class GuiStatBarCapability extends GuiStatBar {
-
     private static final int efficiencyMax = 50;
 
     private GuiCapability capabilityElement;
     private IStatGetter levelGetter;
+
+    private GuiTexture strikingIndicator;
+    private IStatGetter strikingGetter;
+
+    private GuiTexture sweepingIndicator;
+    private IStatGetter sweepingGetter;
 
     public GuiStatBarCapability(int x, int y, int width, Capability capability) {
         super(x, y, width, "", 0, efficiencyMax,
@@ -28,6 +38,23 @@ public class GuiStatBarCapability extends GuiStatBar {
         levelGetter = new StatGetterCapabilityLevel(capability);
         capabilityElement = new GuiCapability(-3, -3, capability);
         addChild(capabilityElement);
+
+        if (capability == Capability.axe) {
+            strikingGetter = new StatGetterEffectLevel(ItemEffect.strikingAxe, 1);
+        } else if (capability == Capability.pickaxe) {
+            strikingGetter = new StatGetterEffectLevel(ItemEffect.strikingPickaxe, 1);
+        } else if (capability == Capability.cut) {
+            strikingGetter = new StatGetterEffectLevel(ItemEffect.strikingCut, 1);
+        } else if (capability == Capability.shovel) {
+            strikingGetter = new StatGetterEffectLevel(ItemEffect.strikingShovel, 1);
+        }
+
+        strikingIndicator = new GuiTexture(16, -1, 7, 7, 206, 0, GuiTextures.workbench);
+        addChild(strikingIndicator);
+
+        sweepingGetter = new StatGetterEffectLevel(ItemEffect.sweepingStrike, 1);
+        sweepingIndicator = new GuiTexture(16, -1, 7, 7, 213, 0, GuiTextures.workbench);
+        addChild(sweepingIndicator);
     }
 
     @Override
@@ -43,12 +70,54 @@ public class GuiStatBarCapability extends GuiStatBar {
             color = getDiffColor(level, previewLevel);
             level = previewLevel;
         } else if (slot != null) {
-            int previewLevel = level - getSlotLevel(player, currentStack, slot, improvement);;
+            int previewLevel = level - getSlotLevel(player, currentStack, slot, improvement);
 
             color = getDiffColor(previewLevel, level);
         }
 
         capabilityElement.update(level, color);
+
+        updateIndicators(player, currentStack, previewStack, slot, improvement);
+    }
+
+    private void updateIndicators(PlayerEntity player, ItemStack currentStack, ItemStack previewStack, String slot, String improvement) {
+        int currentStriking = strikingGetter != null ? (int) strikingGetter.getValue(player, currentStack) : 0;
+        int previewStriking = currentStriking;
+
+        int currentSweeping = (int) sweepingGetter.getValue(player, currentStack);
+        int previewSweeping = currentSweeping;
+
+        if (!previewStack.isEmpty()) {
+            previewStriking -= strikingGetter != null ? (int) strikingGetter.getValue(player, previewStack) : 0;
+            previewSweeping -= (int) sweepingGetter.getValue(player, previewStack);
+        } else if (slot != null) {
+            if (improvement != null) {
+                previewStriking -= strikingGetter != null ? (int) strikingGetter.getValue(player, currentStack, slot, improvement) : 0;
+                previewSweeping -= (int) sweepingGetter.getValue(player, currentStack, slot, improvement);
+            } else {
+                previewStriking -= strikingGetter != null ? (int) strikingGetter.getValue(player, currentStack, slot) : 0;
+                previewSweeping -= (int) sweepingGetter.getValue(player, currentStack, slot);
+
+            }
+        }
+
+        if (currentStriking > 0 || previewStriking > 0) {
+            if (currentSweeping > 0 || previewSweeping > 0) {
+                strikingIndicator.setVisible(false);
+                sweepingIndicator.setVisible(true);
+
+                sweepingIndicator.setColor(getDiffColor(previewSweeping, currentSweeping));
+            } else {
+                strikingIndicator.setVisible(true);
+                sweepingIndicator.setVisible(false);
+
+                strikingIndicator.setColor(getDiffColor(previewStriking, currentStriking));
+            }
+        } else {
+            strikingIndicator.setVisible(false);
+            sweepingIndicator.setVisible(false);
+        }
+
     }
 
     @Override
@@ -58,9 +127,19 @@ public class GuiStatBarCapability extends GuiStatBar {
         if (GuiAlignment.left.equals(alignment)) {
             bar.setX(16);
             capabilityElement.setX(-3);
+
+            strikingIndicator.setX(16);
+            strikingIndicator.setAttachment(alignment.toAttachment());
+            sweepingIndicator.setX(16);
+            sweepingIndicator.setAttachment(alignment.toAttachment());
         } else {
             bar.setX(0);
             capabilityElement.setX(0);
+
+            strikingIndicator.setX(-16);
+            strikingIndicator.setAttachment(alignment.toAttachment());
+            sweepingIndicator.setX(-16);
+            sweepingIndicator.setAttachment(alignment.toAttachment());
         }
 
         capabilityElement.setAttachment(alignment.toAttachment());
