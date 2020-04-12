c67141fd9d0a7ed578bc6548bee3ec8f2abe4bbd
Mikael E. Wikner <wikner.mikael@gmail.com>
2020-04-12 19:20:56 +0200

    1.15 fix hammer head rendering

--- src/main/java/se/mickelus/tetra/blocks/forged/hammer/HammerHeadTESR.java
+++ src/main/java/se/mickelus/tetra/blocks/forged/hammer/HammerHeadTESR.java
@@ -1,12 +1,19 @@
 package se.mickelus.tetra.blocks.forged.hammer;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.Atlases;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.chunk.ChunkRenderCache;
+import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.client.model.data.EmptyModelData;
 
 @OnlyIn(Dist.CLIENT)
 public class HammerHeadTESR extends TileEntityRenderer<HammerHeadTile> {
@@ -15,23 +22,22 @@ public class HammerHeadTESR extends TileEntityRenderer<HammerHeadTile> {
 
     public HammerHeadTESR(TileEntityRendererDispatcher rendererDispatcher) {
         super(rendererDispatcher);
+
+        blockRenderer = Minecraft.getInstance().getBlockRendererDispatcher();
     }
 
     // todo 1.15: ripped out
     @Override
-    public void render(HammerHeadTile hammerHeadTile, float v, MatrixStack matrixStack, IRenderTypeBuffer iRenderTypeBuffer, int i, int i1) {
-//        if(blockRenderer == null) {
-//            blockRenderer = Minecraft.getInstance().getBlockRendererDispatcher();
-//        }
-//
-//        BlockPos pos = te.getPos();
-//        IEnviromentBlockReader world = MinecraftForgeClient.getRegionRenderCache(te.getWorld(), pos);
-//        IBakedModel model = blockRenderer.getBlockModelShapes().getModel(HammerHeadBlock.instance.getDefaultState());
-//
-//        double offset = Math.min(1, Math.max(0, (1d * System.currentTimeMillis() - te.getActivationTime()) / animationDuration)) - 0.125;
-//        buffer.setTranslation(x - pos.getX(), y - pos.getY() + offset, z - pos.getZ());
-//
-//        blockRenderer.getBlockModelRenderer().renderModel(world, model, HammerHeadBlock.instance.getDefaultState(), pos, buffer,
-//                false, new Random(), 42, EmptyModelData.INSTANCE);
+    public void render(HammerHeadTile tile, float v, MatrixStack matrixStack, IRenderTypeBuffer buffer,
+            int combinedLight, int combinedOverlay) {
+
+        IBakedModel model = blockRenderer.getBlockModelShapes().getModel(HammerHeadBlock.instance.getDefaultState());
+
+        double offset = Math.min(1, Math.max(0, (1d * System.currentTimeMillis() - tile.getActivationTime()) / animationDuration)) - 0.125;
+        matrixStack.translate(0, offset, 0);
+
+        blockRenderer.getBlockModelRenderer().renderModel(matrixStack.getLast(), buffer.getBuffer(Atlases.getSolidBlockType()),
+                HammerHeadBlock.instance.getDefaultState(), model, 1f, 1f, 1f, combinedLight, combinedOverlay,
+                EmptyModelData.INSTANCE);
     }
 }
