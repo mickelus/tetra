858ed80e1031a74cea883a9c1b64acc61280718e
Mikael E. Wikner <wikner.mikael@gmail.com>
2020-08-13 18:59:56 +0200

    fix: fixed wonky behaviour for tag materials (for real this time?)

--- src/main/java/se/mickelus/tetra/module/schematic/OutcomeMaterial.java
+++ src/main/java/se/mickelus/tetra/module/schematic/OutcomeMaterial.java
@@ -2,19 +2,28 @@ package se.mickelus.tetra.module.schematic;
 
 import com.google.gson.*;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.advancements.criterion.EnchantmentPredicate;
 import net.minecraft.advancements.criterion.ItemPredicate;
+import net.minecraft.advancements.criterion.MinMaxBounds;
+import net.minecraft.advancements.criterion.NBTPredicate;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.potion.Potion;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.TagCollection;
 import net.minecraft.tags.TagCollectionManager;
 import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -34,11 +43,76 @@ public class OutcomeMaterial {
     private static final Logger logger = LogManager.getLogger();
 
     private ItemPredicate predicate;
-    private LazyOptional<ItemPredicate> lazyPredicate;
     public int count = 1;
 
     private ItemStack itemStack;
     private ResourceLocation tagLocation;
+
+    @OnlyIn(Dist.CLIENT)
+    public ITextComponent[] getDisplayNames() {
+        if (getPredicate() == null) {
+            return new ITextComponent[] {new StringTextComponent("Unknown material")};
+        } else if (itemStack != null) {
+            return new ITextComponent[] {itemStack.getDisplayName()};
+        } else if (tagLocation != null) {
+            return getTagCollection()
+                    .getOrCreate(tagLocation)
+                    .getAllElements()
+                    .stream()
+                    .map(item -> item.getDisplayName(item.getDefaultInstance()))
+                    .toArray(ITextComponent[]::new);
+        }
+
+        return new ITextComponent[] {new StringTextComponent("Unknown material")};
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public ItemStack[] getApplicableItemStacks() {
+        if (getPredicate() == null) {
+            return new ItemStack[0];
+        } else if (itemStack != null && !itemStack.isEmpty()) {
+            return new ItemStack[] { itemStack };
+        } else if (tagLocation != null) {
+            return getTagCollection()
+                    .getOrCreate(tagLocation)
+                    .getAllElements()
+                    .stream()
+                    .map(Item::getDefaultInstance)
+                    .map(this::setCount)
+                    .toArray(ItemStack[]::new);
+        }
+
+        return new ItemStack[0];
+    }
+
+    // todo: this is rather hacky, networked tags are required on clients in multiplayer, but that's not always available
+    private static TagCollection<Item> getTagCollection() {
+        if (FMLEnvironment.dist.isClient()) {
+            if (Minecraft.getInstance().world != null) {
+                return Minecraft.getInstance().world.getTags().getItems();
+            }
+        }
+
+        return TagCollectionManager.func_232928_e_().func_232925_b_();
+    }
+
+    @Nullable
+    public ItemPredicate getPredicate() {
+        return predicate;
+    }
+
+    private ItemStack setCount(ItemStack itemStack) {
+        itemStack.setCount(count);
+        return itemStack;
+    }
+
+    public boolean isTagged() {
+        return tagLocation != null;
+    }
+
+    public boolean isValid() {
+        return predicate != null;
+    }
 
     public static class Deserializer implements JsonDeserializer<OutcomeMaterial> {
 
@@ -68,77 +142,34 @@ public class OutcomeMaterial {
                     material.tagLocation = new ResourceLocation(JSONUtils.getString(jsonObject, "tag"));
                 }
 
-                material.lazyPredicate = LazyOptional.of(() -> {
-                    try {
-                        return ItemPredicate.deserialize(element);
-                    } catch (JsonParseException e) {
-                        // skips setting craft predicate, material will be treated as invalid
-                        logger.debug("Failed to parse outcome material predicate for \"{}\": {}", jsonObject.toString(), e.getMessage());
-                        throw e;
-                    }
-                });
-
+                if (!jsonObject.has("type") && jsonObject.has("tag")) {
+                    material.predicate = deserializeTagPredicate(element);
+                } else {
+                    material.predicate = ItemPredicate.deserialize(element);
+                }
             }
             return material;
         }
-    }
 
-    @OnlyIn(Dist.CLIENT)
-    public ITextComponent[] getDisplayNames() {
-        if (getPredicate() == null) {
-            return new ITextComponent[] {new StringTextComponent("Unknown material")};
-        } else if (itemStack != null) {
-            return new ITextComponent[] {itemStack.getDisplayName()};
-        } else if (tagLocation != null) {
-            return TagCollectionManager.func_232928_e_().func_232925_b_()
-                    .getOrCreate(tagLocation)
-                    .getAllElements()
-                    .stream()
-                    .map(item -> item.getDisplayName(item.getDefaultInstance()))
-                    .toArray(ITextComponent[]::new);
+        // todo: workaround as vanilla predicates always use the non-networked tag manager 
+        private ItemPredicate deserializeTagPredicate(@Nullable JsonElement element) {
+            JsonObject jsonObject = JSONUtils.getJsonObject(element, "item");
+
+            ResourceLocation resourceLocation = new ResourceLocation(JSONUtils.getString(jsonObject, "tag"));
+            ITag<Item> tag = getTagCollection().get(resourceLocation);
+
+            if (tag == null) {
+                logger.debug("Failed to parse outcome material predicate for \"{}\": Unknown tag '{}'", jsonObject.toString(), resourceLocation);
+                return null;
+            }
+
+            MinMaxBounds.IntBound count = MinMaxBounds.IntBound.fromJson(jsonObject.get("count"));
+            MinMaxBounds.IntBound durability = MinMaxBounds.IntBound.fromJson(jsonObject.get("durability"));
+            EnchantmentPredicate[] enchantments = EnchantmentPredicate.deserializeArray(jsonObject.get("enchantments"));
+            EnchantmentPredicate[] storedEnchantments = EnchantmentPredicate.deserializeArray(jsonObject.get("stored_enchantments"));
+            NBTPredicate nbt = NBTPredicate.deserialize(jsonObject.get("nbt"));
+
+            return new ItemPredicate(tag, null, count, durability, enchantments, storedEnchantments, null, nbt);
         }
-
-        return new ITextComponent[] {new StringTextComponent("Unknown material")};
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public ItemStack[] getApplicableItemStacks() {
-        if (getPredicate() == null) {
-            return new ItemStack[0];
-        } else if (itemStack != null && !itemStack.isEmpty()) {
-            return new ItemStack[] { itemStack };
-        } else if (tagLocation != null) {
-            return TagCollectionManager.func_232928_e_().func_232925_b_()
-                    .getOrCreate(tagLocation)
-                    .getAllElements()
-                    .stream()
-                    .map(Item::getDefaultInstance)
-                    .map(this::setCount)
-                    .toArray(ItemStack[]::new);
-        }
-
-        return new ItemStack[0];
-    }
-
-    @Nullable
-    public ItemPredicate getPredicate() {
-        try {
-            return lazyPredicate.orElse(null);
-        } catch (JsonParseException e) {
-            return null;
-        }
-    }
-
-    private ItemStack setCount(ItemStack itemStack) {
-        itemStack.setCount(count);
-        return itemStack;
-    }
-
-    public boolean isTagged() {
-        return tagLocation != null;
-    }
-
-    public boolean isValid() {
-        return isTagged() || itemStack != null;
     }
 }
