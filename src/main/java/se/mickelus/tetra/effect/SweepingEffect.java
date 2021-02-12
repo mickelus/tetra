4a3ef0a9aba999fa1e115814491deab78718e5d4
Mikael E. Wikner <wikner.mikael@gmail.com>
2021-02-12 04:32:47 +0100

    fix: fixed sweeping range being incredibly short

--- src/main/java/se/mickelus/tetra/effect/SweepingEffect.java
+++ src/main/java/se/mickelus/tetra/effect/SweepingEffect.java
@@ -41,7 +41,7 @@ public class SweepingEffect {
                 .filter(entity -> entity != attacker)
                 .filter(entity -> entity != target)
                 .filter(entity -> !attacker.isOnSameTeam(entity))
-                .filter(entity -> attacker.getDistanceSq(entity) < range * range)
+                .filter(entity -> attacker.getDistanceSq(entity) < (range + reach) * (range + reach))
                 .forEach(entity -> {
                     entity.applyKnockback(knockback,
                             MathHelper.sin(attacker.rotationYaw * (float) Math.PI / 180F),
