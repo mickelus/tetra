fba81dbed70d187e1d6e9aa895d8d6a293f87ace
Mikael E. Wikner <wikner.mikael@gmail.com>
2021-06-16 20:45:39 +0200

    fix: possibly fixed crossmod crashes caused by the armor penetration effect

--- src/main/java/se/mickelus/tetra/effect/ArmorPenetrationEffect.java
+++ src/main/java/se/mickelus/tetra/effect/ArmorPenetrationEffect.java
@@ -13,11 +13,18 @@ public class ArmorPenetrationEffect {
 
     private static final UUID uuid = UUID.fromString("a43e0407-f070-4e2f-8813-a5e16328f1a5");
 
+    /**
+     * Applies the armor reduction effect before the damage value is calculated.
+     * Note that some mods cause this to be called twice before onLivingDamage.
+     * @param event
+     * @param effectLevel
+     */
     public static void onLivingHurt(LivingHurtEvent event, int effectLevel) {
         Optional.of(event.getEntityLiving())
                 .map(LivingEntity::getAttributeManager)
                 .filter(manager -> manager.hasAttributeInstance(Attributes.ARMOR))
                 .map(manager -> manager.createInstanceIfAbsent(Attributes.ARMOR))
+                .filter(instance -> instance.getModifier(uuid) == null)
                 .ifPresent(instance -> instance.applyNonPersistentModifier(
                         new AttributeModifier(uuid, "tetra_armor_pen", effectLevel * -0.01, AttributeModifier.Operation.MULTIPLY_TOTAL)));
     }
