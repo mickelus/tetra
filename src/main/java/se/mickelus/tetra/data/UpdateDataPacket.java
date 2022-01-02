0677d914f436fc5519838254a897f5356fa1d7de
Mikael E. Wikner <wikner.mikael@gmail.com>
2022-01-03 00:29:45 +0100

    moved util classes to mutil

--- src/main/java/se/mickelus/tetra/data/UpdateDataPacket.java
+++ src/main/java/se/mickelus/tetra/data/UpdateDataPacket.java
@@ -1,52 +1,20 @@
 package se.mickelus.tetra.data;
 
 import com.google.gson.JsonElement;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-import se.mickelus.mutil.network.AbstractPacket;
+import se.mickelus.mutil.data.AbstractUpdateDataPacket;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @ParametersAreNonnullByDefault
-public class UpdateDataPacket extends AbstractPacket {
-
-    private String directory;
-    private Map<ResourceLocation, String> data;
-
+public class UpdateDataPacket extends AbstractUpdateDataPacket {
     public UpdateDataPacket() {
     }
 
     public UpdateDataPacket(String directory, Map<ResourceLocation, JsonElement> data) {
-        this.directory = directory;
-        this.data = data.entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().toString()
-                ));
-    }
-
-    @Override
-    public void toBytes(FriendlyByteBuf buffer) {
-        buffer.writeUtf(directory);
-        buffer.writeInt(data.size());
-        data.forEach((resourceLocation, data) -> {
-            buffer.writeResourceLocation(resourceLocation);
-            buffer.writeUtf(data);
-        });
-    }
-
-    @Override
-    public void fromBytes(FriendlyByteBuf buffer) {
-        directory = buffer.readUtf();
-        int count = buffer.readInt();
-        data = new HashMap<>();
-        for (int i = 0; i < count; i++) {
-            data.put(buffer.readResourceLocation(), buffer.readUtf());
-        }
+        super(directory, data);
     }
 
     @Override
--- src/main/java/se/mickelus/tetra/data/deserializer/ModuleModelDeserializer.java
+++ src/main/java/se/mickelus/tetra/data/deserializer/ModuleModelDeserializer.java
@@ -1,6 +1,7 @@
 package se.mickelus.tetra.data.deserializer;
 
 import com.google.gson.*;
+import se.mickelus.mutil.data.deserializer.ResourceLocationDeserializer;
 import se.mickelus.tetra.items.modular.ItemColors;
 import se.mickelus.tetra.module.data.ModuleModel;
 
