89a606107da9b00bf1609669c0fba8366e4bb883
Mikael E. Wikner <wikner.mikael@gmail.com>
2022-08-30 02:41:46 +0200

    added tooltip describing where to find dragon sinews

--- src/main/java/se/mickelus/tetra/items/loot/DragonSinewItem.java
+++ src/main/java/se/mickelus/tetra/items/loot/DragonSinewItem.java
@@ -1,36 +1,47 @@
 package se.mickelus.tetra.items.loot;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootTableReference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.ObjectHolder;
 import se.mickelus.tetra.TetraMod;
 import se.mickelus.tetra.items.TetraItem;
 import se.mickelus.tetra.items.TetraItemGroup;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 @ParametersAreNonnullByDefault
 public class DragonSinewItem extends TetraItem {
-    public static final String unlocalizedName = "dragon_sinew";
+    public static final String identifier = "dragon_sinew";
+    static final Component tooltip = Component.translatable("item.tetra." + identifier + ".description")
+            .withStyle(ChatFormatting.GRAY);
     private static final ResourceLocation dragonLootTable = new ResourceLocation("entities/ender_dragon");
     private static final ResourceLocation sinewLootTable = new ResourceLocation(TetraMod.MOD_ID, "entities/ender_dragon_extended");
-    @ObjectHolder(registryName = "item", value = TetraMod.MOD_ID + ":" + unlocalizedName)
-    public static DragonSinewItem instance;
 
     public DragonSinewItem() {
         super(new Properties().tab(TetraItemGroup.instance));
 
         MinecraftForge.EVENT_BUS.register(new LootTableHandler());
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(DragonSinewItem.tooltip);
     }
 
     @Override
@@ -61,7 +72,7 @@ public class DragonSinewItem extends TetraItem {
         public void onLootTableLoad(final LootTableLoadEvent event) {
             if (event.getName().equals(dragonLootTable)) {
                 event.getTable().addPool(LootPool.lootPool()
-                        .name(TetraMod.MOD_ID + ":" + unlocalizedName)
+                        .name(TetraMod.MOD_ID + ":" + identifier)
                         .add(LootTableReference.lootTableReference(sinewLootTable)).build());
             }
         }
