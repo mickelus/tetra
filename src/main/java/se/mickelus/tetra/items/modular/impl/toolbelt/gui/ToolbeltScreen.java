924ca9635eaffa4d924043a8f3e84fa746fcc132
Mikael E. Wikner <wikner.mikael@gmail.com>
2022-10-11 01:04:48 +0200

    fix: fixed toolbelt inventories not working when the toolbelt is in the first slot

--- src/main/java/se/mickelus/tetra/items/modular/impl/toolbelt/gui/ToolbeltScreen.java
+++ src/main/java/se/mickelus/tetra/items/modular/impl/toolbelt/gui/ToolbeltScreen.java
@@ -90,7 +90,7 @@ public class ToolbeltScreen extends AbstractContainerScreen<ToolbeltContainer> {
     @Override
     protected void slotClicked(Slot slot, int slotIndex, int barIndex, ClickType clickType) {
         // todo: based on how quick swapping is implemented in AbstractContainerMenu.doClick, there has to be a cleaner way
-        if (!(slot instanceof DisabledSlot || minecraft.player.getInventory().getItem(barIndex).getItem() instanceof ModularToolbeltItem)) {
+        if (!(slot instanceof DisabledSlot || minecraft.player.getInventory().getItem(slotIndex).getItem() instanceof ModularToolbeltItem)) {
             super.slotClicked(slot, slotIndex, barIndex, clickType);
         }
     }
