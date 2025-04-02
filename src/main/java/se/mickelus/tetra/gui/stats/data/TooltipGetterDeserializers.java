8c55088ba25b15da86b91a56381b79c6c1e50344
Mikael E. Wikner <wikner.mikael@gmail.com>
2025-04-02 17:52:09 +0200

    tech: made it possible to omit the stats and formatters fields for stat bar tooltips when no data is to be displayed in the tooltip

--- src/main/java/se/mickelus/tetra/gui/stats/data/TooltipGetterDeserializers.java
+++ src/main/java/se/mickelus/tetra/gui/stats/data/TooltipGetterDeserializers.java
@@ -9,7 +9,9 @@ import se.mickelus.tetra.gui.stats.getter.TooltipGetterMultiValue;
 public class TooltipGetterDeserializers {
     public static ITooltipGetter defaultGetter(JsonElement json) {
         StandardData data = StatRegistry.gson.fromJson(json, StandardData.class);
-        return new TooltipGetterMultiValue(data.key, data.stats, data.formatters);
+        return new TooltipGetterMultiValue(data.key,
+                data.stats != null ? data.stats : new IStatGetter[0],
+                data.formatters != null ? data.formatters : new IStatFormat[0]);
     }
 
     record StandardData(String key, IStatGetter[] stats, IStatFormat[] formatters) {
