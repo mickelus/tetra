f85dab4ef4ec5d6072547cec4c729b9c39ba4eb9
Mikael E. Wikner <wikner.mikael@gmail.com>
2025-07-30 22:25:36 +0200

    tech: added explosion crafting effect

---
+++ src/main/java/se/mickelus/tetra/craftingeffect/outcome/ExplosionOutcome.java
@@ -0,0 +1,44 @@
+package se.mickelus.tetra.craftingeffect.outcome;
+
+import java.util.Map;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+import se.mickelus.tetra.module.schematic.UpgradeSchematic;
+
+public class ExplosionOutcome implements CraftingEffectOutcome {
+    float chance = 1;
+    float radius = 4;
+    int randomOriginDistance = 0;
+    Level.ExplosionInteraction type = Level.ExplosionInteraction.NONE;
+
+    @Override
+    public boolean apply(final ResourceLocation[] unlockedEffects, final ItemStack upgradedStack, final String slot, final boolean isReplacing,
+            final Player player, final ItemStack[] preMaterials, final Map<ToolAction, Integer> tools, final Level world,
+            final UpgradeSchematic schematic, final BlockPos pos, final BlockState blockState, final boolean consumeResources,
+            final ItemStack[] postMaterials) {
+        if (consumeResources && !world.isClientSide()) {
+            BlockPos origin = pos.above();
+            if (randomOriginDistance > 0) {
+                origin = getRandomBlockPos(world.random, randomOriginDistance).offset(origin);
+            }
+
+            if (world.random.nextDouble() < chance) {
+                world.explode(player, origin.getX(), origin.getY(), origin.getZ(), radius, type);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static BlockPos getRandomBlockPos(RandomSource random, int radius) {
+        return new BlockPos(random.nextIntBetweenInclusive(-radius, radius),
+                random.nextIntBetweenInclusive(-radius, radius),
+                random.nextIntBetweenInclusive(-radius, radius));
+    }
+}
