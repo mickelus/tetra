76b79d1dd7b50c911e7fea999482d94b5f2ed2e1
Mikael E. Wikner <wikner.mikael@gmail.com>
2025-12-07 23:28:15 +0100

    tech: added translation entries for overriding improvement texts in holosphere the material UI

--- src/main/java/se/mickelus/tetra/items/modular/impl/holo/gui/craft/material/HoloMaterialImprovementGui.java
+++ src/main/java/se/mickelus/tetra/items/modular/impl/holo/gui/craft/material/HoloMaterialImprovementGui.java
@@ -2,6 +2,7 @@ package se.mickelus.tetra.items.modular.impl.holo.gui.craft.material;
 
 import com.google.common.collect.ImmutableList;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import se.mickelus.mutil.gui.*;
@@ -10,6 +11,7 @@ import se.mickelus.tetra.gui.GuiTextures;
 import se.mickelus.tetra.items.modular.IModularItem;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Arrays;
 import java.util.List;
 
 @ParametersAreNonnullByDefault
@@ -20,14 +22,29 @@ public class HoloMaterialImprovementGui extends GuiElement {
     protected GuiString value;
 
     List<Component> tooltip;
+    List<Component> extendedTooltip;
 
     public HoloMaterialImprovementGui(int x, int y, String key, boolean current, boolean preview) {
         super(x, y, 29, 29);
 
-        String improvementName = IModularItem.getImprovementName(key, 0, null);
+        String improvementName = I18n.exists("tetra.holo.craft.materials.stat_modifier." + key)
+                ? I18n.get("tetra.holo.craft.materials.stat_modifier." + key)
+                : IModularItem.getImprovementName(key, 0, null);
 
-        tooltip = ImmutableList.of(Component.translatable("tetra.holo.craft.materials.stat_modifier.tooltip", improvementName),
-                Component.literal(IModularItem.getImprovementDescription(key, 0, null)).withStyle(ChatFormatting.DARK_GRAY));
+        if (I18n.exists("tetra.holo.craft.materials.stat_modifier." + key + ".tooltip")) {
+            tooltip = Arrays.stream(I18n.get("tetra.holo.craft.materials.stat_modifier." + key + ".tooltip").split("\\n"))
+                    .map(Component::literal)
+                    .collect(ImmutableList.toImmutableList());
+        } else {
+            tooltip = ImmutableList.of(Component.translatable("tetra.holo.craft.materials.stat_modifier.tooltip", improvementName),
+                    Component.literal(IModularItem.getImprovementDescription(key, 0, null)).withStyle(ChatFormatting.DARK_GRAY));
+        }
+
+        if (I18n.exists("tetra.holo.craft.materials.stat_modifier." + key + ".tooltip_extended")) {
+            extendedTooltip = Arrays.stream(I18n.get("tetra.holo.craft.materials.stat_modifier." + key + ".tooltip_extended").split("\\n"))
+                    .map(Component::literal)
+                    .collect(ImmutableList.toImmutableList());
+        }
 
         backdrop = new GuiTexture(0, 0, 29, 29, 97, 0, GuiTextures.workbench);
         backdrop.setColor(0x222222);
@@ -49,6 +66,12 @@ public class HoloMaterialImprovementGui extends GuiElement {
 
     @Override
     public List<Component> getTooltipLines() {
-        return hasFocus() ? tooltip : null;
+        if (hasFocus()) {
+            if (Screen.hasShiftDown() && extendedTooltip != null) {
+                return extendedTooltip;
+            }
+            return tooltip;
+        }
+        return null;
     }
 }
