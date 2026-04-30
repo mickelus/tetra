2ab0dddafb25401972bb2a6eebc660df05f32885
Veselin Plochev <wfphantom5@gmail.com>
2026-05-01 00:47:17 +0300

    bring back comment and add nonnull annotation

--- src/main/java/se/mickelus/tetra/advancements/GenericTrigger.java
+++ src/main/java/se/mickelus/tetra/advancements/GenericTrigger.java
@@ -3,6 +3,7 @@ package se.mickelus.tetra.advancements;
 import com.mojang.serialization.Codec;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.function.Predicate;
@@ -16,10 +17,16 @@ public class GenericTrigger<T extends SimpleCriterionTrigger.SimpleInstance> ext
     }
 
     @Override
-    public Codec<T> codec() {
+    public @NotNull Codec<T> codec() {
         return codec;
     }
 
+    /**
+     * Fulfills all criterion instances that pass the validation predicate.
+     *
+     * @param player              The player that the criterion is to be fulfilled for
+     * @param validationPredicate A predicate used to check which criterion will be fulfilled
+     */
     public void fulfillCriterion(ServerPlayer player, Predicate<T> validationPredicate) {
         trigger(player, validationPredicate);
     }
